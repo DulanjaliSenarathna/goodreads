f80e151eef64e58b8834cc4317b4b6649ab58011
Dulanjali-Senarathna <dishanisenarathna96@gmail.com>
2021-07-14 16:47:48 +0530

    Welcome Screen Done

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kBlackColor = Color(0xFF393939);
+const kLightBlackColor = Color(0xFF8F8F8F);
+const kIconColor = Color(0xFFF48A37);
+const kProgressIndicator = Color(0xFFBE7066);
